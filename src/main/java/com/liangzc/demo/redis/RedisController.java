6cba423d4d32967ee11cdf4c9fffc409b55b0061
liangzc <liangzhichao@baozhentianding.com>
2025-12-15 17:31:30 +0800

    引入RedisSion实现抽奖

--- src/main/java/com/liangzc/demo/redis/RedisController.java
+++ src/main/java/com/liangzc/demo/redis/RedisController.java
@@ -1,16 +1,19 @@
 package com.liangzc.demo.redis;
 
 
+import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @RequestMapping("/redis")
 @RestController
@@ -38,6 +41,33 @@ public class RedisController {
 
         redisTemplate.opsForHash().put("user", id, value);
         return "success";
+    }
+
+    @GetMapping("/redisson/draw")
+    public String redissontest() {
+        List<String> list = Arrays.asList("小明", "小李", "乐乐", "波比", "提莫", "海龟", "小于", "小萨");
+        redissonClient.getSet("user").clear();
+        boolean user = redissonClient.getSet("user").addAll(list);
+        if ( user){
+            log.info("添加成功");
+            log.info("开始抽奖");
+            log.info("=========================可重复抽奖======================");
+            Set<Object> user1 = redissonClient.getSet("user").random(3);
+            log.info("三等奖中奖用户：{}", JSON.toJSONString(user1));
+            Set<Object> user2 = redissonClient.getSet("user").random(2);
+            log.info("二等奖中奖用户：{}", JSON.toJSONString(user2));
+            Set<Object> user3 = redissonClient.getSet("user").random(1);
+            log.info("一等奖中奖用户：{}", JSON.toJSONString(user3));
+
+            log.info("=========================不可重复抽奖======================");
+            Set<Object> user4 = redissonClient.getSet("user").removeRandom(3);
+            log.info("三等奖中奖用户：{}", JSON.toJSONString(user4));
+            Set<Object> user5 = redissonClient.getSet("user").removeRandom(2);
+            log.info("二等奖中奖用户：{}", JSON.toJSONString(user5));
+            Set<Object> user6 = redissonClient.getSet("user").removeRandom(1);
+            log.info("一等奖中奖用户：{}", JSON.toJSONString(user6));
+        }
+        return "";
     }
 
 
